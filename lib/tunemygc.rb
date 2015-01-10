2fd40600c317a9fb7a4298e5281227af54708600
Lourens Naudé <lourens@methodmissing.com>
2015-01-10 21:12:49 +0000

    Note when not enabled

--- lib/tunemygc.rb
+++ lib/tunemygc.rb
@@ -5,6 +5,8 @@ tunemygc_min_ruby_version = "2.1.0"
 if RUBY_VERSION >= tunemygc_min_ruby_version
   if ENV["RUBY_GC_TUNE"] && defined?(Rails) && Rails.version >= "4.0"
     require 'tunemygc/railtie'
+  else
+    puts "[TuneMyGC] not enabled"
   end
 else
   puts "[TuneMyGC] requires a Ruby version #{tunemygc_min_ruby_version} or newer"
