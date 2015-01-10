76dac4d2bb932c391ab4e14977b1ab21e1389bae
Lourens Naudé <lourens@methodmissing.com>
2015-01-10 22:49:14 +0000

    Let the configurator write to foreman compatible .env files

--- lib/tunemygc/configurator.rb
+++ lib/tunemygc/configurator.rb
@@ -10,17 +10,20 @@ module TuneMyGc
 
     def configure
       if Hash === config && !config.empty?
-        TuneMyGc.log "==== Recommended GC config (#{config.delete("callback")}) ===="
-        memory = config.delete("Memory")
-        TuneMyGc.log "== Optimize for memory"
-        memory.each do |var,val|
-          TuneMyGc.log "#{var}=#{val}"
+        TuneMyGc.log "==== Recommended GC config from #{config.delete("callback")} ===="
+        TuneMyGc.log "== Wrote #{write_env("Memory")}"
+        TuneMyGc.log "== Wrote #{write_env("Speed")}"
+      end
+
+      private
+      def write_env(strategy)
+        path = File.join(Rails.root, "tunemygc-#{strategy.downcase}.env")
+        File.open(path) do |f|
+          config.delete(strategy).each do |var,val|
+            f.write "#{var}=#{val}"
+          end
         end
-        speed = config.delete("Speed")
-        TuneMyGc.log "== Optimize for speed"
-        speed.each do |var,val|
-          TuneMyGc.log "#{var}=#{val}"
-        end
+        path
       end
     end
   end
