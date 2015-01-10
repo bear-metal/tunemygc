8e5ea58fc58d121f767acc4a27de47bf15c0737f
Lourens Naudé <lourens@methodmissing.com>
2015-01-10 23:00:51 +0000

    Open environment files for writing

--- lib/tunemygc/configurator.rb
+++ lib/tunemygc/configurator.rb
@@ -19,7 +19,7 @@ module TuneMyGc
     private
     def write_env(strategy)
       path = File.join(Rails.root, "tunemygc-#{strategy.downcase}.env")
-      File.open(path) do |f|
+      File.open(path, "w") do |f|
         config.delete(strategy).each do |var,val|
           f.write "#{var}=#{val}"
         end
