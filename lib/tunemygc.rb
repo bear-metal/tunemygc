b3f7c2fa0a178d2130d5fb1d0995392b5b0be77e
Lourens Naudé <lourens@methodmissing.com>
2015-01-10 14:59:30 +0000

    Relax restriction for min. Ruby version to 2.1.0

--- lib/tunemygc.rb
+++ lib/tunemygc.rb
@@ -2,7 +2,7 @@
 
 tunemygc_min_ruby_version = "2.1.0"
 
-if RUBY_VERSION > tunemygc_min_ruby_version
+if RUBY_VERSION >= tunemygc_min_ruby_version
   if ENV["RUBY_GC_TUNE"] && defined?(Rails) && Rails.version >= "4.0"
     require 'tunemygc/railtie'
   end
