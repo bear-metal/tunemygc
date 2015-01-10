2471bc83839065589b9e3fdd2bfad16c41dd6ac7
Lourens Naudé <lourens@methodmissing.com>
2015-01-10 22:54:52 +0000

    Fix typo

--- lib/tunemygc/configurator.rb
+++ lib/tunemygc/configurator.rb
@@ -14,17 +14,17 @@ module TuneMyGc
         TuneMyGc.log "== Wrote #{write_env("Memory")}"
         TuneMyGc.log "== Wrote #{write_env("Speed")}"
       end
+    end
 
-      private
-      def write_env(strategy)
-        path = File.join(Rails.root, "tunemygc-#{strategy.downcase}.env")
-        File.open(path) do |f|
-          config.delete(strategy).each do |var,val|
-            f.write "#{var}=#{val}"
-          end
+    private
+    def write_env(strategy)
+      path = File.join(Rails.root, "tunemygc-#{strategy.downcase}.env")
+      File.open(path) do |f|
+        config.delete(strategy).each do |var,val|
+          f.write "#{var}=#{val}"
         end
-        path
       end
+      path
     end
   end
 end
