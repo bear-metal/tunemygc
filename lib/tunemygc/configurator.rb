3f932845bca7fae9044d435d2dc1be4a8fc691ba
Lourens Naudé <lourens@methodmissing.com>
2015-01-13 22:47:42 +0000

    Also multiplex config suggestions to logger

--- lib/tunemygc/configurator.rb
+++ lib/tunemygc/configurator.rb
@@ -20,11 +20,16 @@ module TuneMyGc
     def write_env(strategy)
       path = File.join(Rails.root, "tunemygc-#{strategy.downcase}.env")
       File.open(path, "w") do |f|
+        TuneMyGc.log "== start #{strategy} config =="
         config.delete(strategy).each do |var,val|
+          TuneMyGc.log "#{var}=#{val}"
           f.write "#{var}=#{val}\n"
         end
+        TuneMyGc.log "== end #{strategy} config =="
       end
       path
+    rescue Exception => e
+      TuneMyGc.log "Failed to write #{strategy} config (#{e.message})"
     end
   end
 end
