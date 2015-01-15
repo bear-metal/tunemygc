a412c99a5fcbbb812b658d897b3297a3e7285630
Lourens Naudé <lourens@methodmissing.com>
2015-01-15 18:35:15 +0000

    Do not write out files, its evil

--- lib/tunemygc/configurator.rb
+++ lib/tunemygc/configurator.rb
@@ -11,25 +11,19 @@ module TuneMyGc
     def configure
       if Hash === config && !config.empty?
         TuneMyGc.log "==== Recommended GC configs from #{config.delete("callback")}"
-        TuneMyGc.log "== Wrote #{write_env("Memory")}"
-        TuneMyGc.log "== Wrote #{write_env("Speed")}"
+        write_env("Memory")
+        write_env("Speed")
       end
     end
 
     private
     def write_env(strategy)
-      path = File.join(Rails.root, "tunemygc-#{strategy.downcase}.env")
-      File.open(path, "w") do |f|
-        TuneMyGc.log "== start #{strategy} config =="
-        config.delete(strategy).each do |var,val|
-          TuneMyGc.log "#{var}=#{val}"
-          f.write "#{var}=#{val}\n"
-        end
-        TuneMyGc.log "== end #{strategy} config =="
+      TuneMyGc.log "== start #{strategy.downcase} config =="
+      config.delete(strategy).each do |var,val|
+        TuneMyGc.log "#{var}=#{val}"
       end
-      path
-    rescue Exception => e
-      TuneMyGc.log "Failed to write #{strategy} config (#{e.message})"
+      TuneMyGc.log "== end #{strategy.downcase} config =="
+      strategy
     end
   end
 end
