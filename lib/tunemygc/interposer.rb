2cf599e8117777e25493f9d8cdee192859dee6a1
Lourens Naudé <lourens@methodmissing.com>
2015-01-23 09:50:51 +0000

    Also pack request ids into controller events

--- lib/tunemygc/interposer.rb
+++ lib/tunemygc/interposer.rb
@@ -18,12 +18,12 @@ module TuneMyGc
       TuneMyGc.snapshot(:BOOTED)
 
       TuneMyGc.interposer.subscriptions << ActiveSupport::Notifications.subscribe(/^start_processing.action_controller$/) do |*args|
-        TuneMyGc.snapshot(:REQUEST_PROCESSING_STARTED, args[1].to_f)
+        TuneMyGc.snapshot(:REQUEST_PROCESSING_STARTED, args[1].to_f, :id => args[3] )
       end
       TuneMyGc.log "hooked: start_processing.action_controller"
 
       TuneMyGc.interposer.subscriptions << ActiveSupport::Notifications.subscribe(/^process_action.action_controller$/) do |*args|
-        TuneMyGc.snapshot(:REQUEST_PROCESSING_ENDED, args[2].to_f)
+        TuneMyGc.snapshot(:REQUEST_PROCESSING_ENDED, args[2].to_f, :id => args[3])
         TuneMyGc.interposer.check_uninstall_request_processing
       end
       TuneMyGc.log "hooked: process_action.action_controller"
