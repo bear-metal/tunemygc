58ad50137922f4ad5180b0ae5f117da04a22293e
Lourens Naudé <lourens@methodmissing.com>
2015-01-28 14:32:04 +0000

    Standardize on a single config strategy

--- lib/tunemygc/configurator.rb
+++ lib/tunemygc/configurator.rb
@@ -11,19 +11,15 @@ module TuneMyGc
     def configure
       if Hash === config && !config.empty?
         TuneMyGc.log "==== Recommended GC configs from #{config.delete("callback")}"
-        write_env("Memory")
         write_env("Speed")
       end
     end
 
     private
     def write_env(strategy)
-      TuneMyGc.log "== start #{strategy.downcase} config =="
       config.delete(strategy).each do |var,val|
-        TuneMyGc.log "#{var}=#{val}"
+        TuneMyGc.log "export #{var}=#{val}"
       end
-      TuneMyGc.log "== end #{strategy.downcase} config =="
-      strategy
     end
   end
 end
