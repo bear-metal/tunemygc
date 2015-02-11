c767a5ee539ad33909db5fa0fe22379f151a5a41
Lourens Naudé <lourens@methodmissing.com>
2015-02-11 20:01:02 +0000

    Move all ActionController specific hooks into spies/action_controller.rb

--- lib/tunemygc/spies/action_controller.rb
+++ lib/tunemygc/spies/action_controller.rb
@@ -1,6 +1,21 @@
 # encoding: utf-8
 
-require 'tunemygc/request_subscriber'
+require 'tunemygc/subscriber'
+
+module TuneMyGc
+  class StartRequestSubscriber < Subscriber
+    def start(name, id, payload)
+      TuneMyGc.snapshot(:PROCESSING_STARTED)
+    end
+  end
+
+  class EndRequestSubscriber < Subscriber
+    def finish(name, id, payload)
+      TuneMyGc.snapshot(:PROCESSING_ENDED)
+      TuneMyGc.interposer.check_uninstall
+    end
+  end
+end
 
 module TuneMyGc
   module Spies
