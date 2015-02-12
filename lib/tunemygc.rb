b66f75863576fa8fc81f40a99e509b3a72a83dcb
Lourens Naudé <lourens@methodmissing.com>
2015-02-12 19:15:38 +0000

    Delegate min ruby version check to gemspec

--- lib/tunemygc.rb
+++ lib/tunemygc.rb
@@ -1,22 +1,16 @@
 # encoding: utf-8
 
-tunemygc_min_ruby_version = "2.1.0"
+require "tunemygc/version" unless defined? TuneMyGc::VERSION
 
-if RUBY_VERSION >= tunemygc_min_ruby_version
-  require "tunemygc/version" unless defined? TuneMyGc::VERSION
+module TuneMyGc
+  HOST = (ENV['RUBY_GC_TUNE_HOST'] || "tunemygc.com:443").freeze
+  HEADERS = { "Content-Type" => "application/json",
+              "Accept" => "application/json",
+              "User-Agent" => "TuneMyGC #{TuneMyGc::VERSION}"}.freeze
+end
 
-  module TuneMyGc
-    HOST = (ENV['RUBY_GC_TUNE_HOST'] || "tunemygc.com:443").freeze
-    HEADERS = { "Content-Type" => "application/json",
-                "Accept" => "application/json",
-                "User-Agent" => "TuneMyGC #{TuneMyGc::VERSION}"}.freeze
-  end
-
-  if ENV["RUBY_GC_TUNE"] && defined?(Rails) && Rails.version >= "4.0"
-    require 'tunemygc/railtie'
-  else
-    puts "[TuneMyGC] not enabled"
-  end
+if ENV["RUBY_GC_TUNE"] && defined?(Rails) && Rails.version >= "4.0"
+  require 'tunemygc/railtie'
 else
-  puts "[TuneMyGC] requires a Ruby version #{tunemygc_min_ruby_version} or newer"
+  puts "[TuneMyGC] not enabled"
 end
