2c7350c36c07d0cb2b946ef1a5ddf67e615b99ad
Lourens Naudé <lourens@methodmissing.com>
2015-03-18 17:25:59 +0000

    Initial pass at sync retry with incremental backoff

--- lib/tunemygc/syncer.rb
+++ lib/tunemygc/syncer.rb
@@ -14,14 +14,41 @@ module TuneMyGc
 
     def sync(snapshotter)
       if sync_required?(snapshotter)
-        response = nil
-        Timeout.timeout(NETWORK_TIMEOUT + 1) do
-          response = sync_with_tuner(snapshotter)
-          process_config_callback(response) if response
+        snapshots = snapshotter.size
+        TuneMyGc.log "Syncing #{snapshots} snapshots"
+        payload = [environment(snapshotter)]
+        debug = ENV["RUBY_GC_TUNE_DEBUG"]
+        TuneMyGc.log "=== Snapshots ===" if debug
+        while !snapshotter.empty?
+          snapshot = snapshotter.deq
+          TuneMyGc.log(snapshot) if debug
+          payload << snapshot
+        end
+        data = ActiveSupport::JSON.encode(payload)
+        begin
+          3.times do |retries|
+            Timeout.timeout(NETWORK_TIMEOUT + 1) do
+              response = sync_with_tuner(data, snapshots)
+              if response
+                if response == :retryable
+                  TuneMyGc.log "Retrying in #{retries} seconds ..."
+                  sleep(retries)
+                else
+                  process_config_callback(response)
+                  break
+                end
+              else
+                break
+              end
+            end
+          end
+          true
+        ensure
+          payload.clear
         end
       else
         TuneMyGc.log "Nothing to sync, discarding #{snapshotter.size} snapshots"
-        snapshotter.clear
+        false
       end
     end
 
@@ -35,22 +62,8 @@ module TuneMyGc
     end
 
     private
-    def sync_with_tuner(snapshotter)
-      snapshots = 0
-      # Fallback to Timeout if Net::HTTP read timeout fails
-      snapshots = snapshotter.size
-      TuneMyGc.log "Syncing #{snapshots} snapshots"
-      payload = [environment(snapshotter)]
-      debug = ENV["RUBY_GC_TUNE_DEBUG"]
-      TuneMyGc.log "=== Snapshots ===" if debug
-      while !snapshotter.empty?
-        snapshot = snapshotter.deq
-        TuneMyGc.log(snapshot) if debug
-        payload << snapshot
-      end
-      data = ActiveSupport::JSON.encode(payload)
+    def sync_with_tuner(data, snapshots)
       response = client.post('/ruby', data, TuneMyGc::HEADERS)
-      snapshotter.unit_of_work = false
       if Net::HTTPNotFound === response
         TuneMyGc.log "Invalid application token. Please generate one with 'bundle exec tunemygc <a_valid_email_address>' and set the RUBY_GC_TOKEN environment variable"
         return false
@@ -67,20 +80,18 @@ module TuneMyGc
         TuneMyGc.log "Invalid payload (#{response.body}). Failed to sync #{snapshots} snapshots"
         return false
       elsif Net::HTTPInternalServerError === response
-        TuneMyGc.log "An internal error occurred (#{response.body}). Failed to sync #{snapshots} snapshots"
-        return false
+        TuneMyGc.log "An internal error occurred (#{response.body}). Failed to sync #{snapshots} snapshots."
+        return :retryable
       elsif Net::HTTPSuccess === response
         response
       else
         TuneMyGc.log "Unknown error: #{response.body}"
         return false
       end
-    rescue Exception => e
+    rescue Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET, Errno::ECONNREFUSED,
+           EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, IOError => e
       TuneMyGc.log "Failed to sync #{snapshots} snapshots (error: #{e})"
-      return false
-    ensure
-      # Prefer to loose data points than accumulate buffers indefinitely on error or other conditions
-      snapshotter.clear
+      return :retryable
     end
 
     def process_config_callback(response)
--- lib/tunemygc/version.rb
+++ lib/tunemygc/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module TuneMyGc
-  VERSION = "1.0.42"
+  VERSION = "1.0.43"
 end
