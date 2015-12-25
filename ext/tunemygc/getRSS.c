c4c53226867356e07dce0caaa285b204661eb326
Lourens Naudé <lourens@methodmissing.com>
2015-12-25 23:35:26 +0000

    Fix missing semicolon in getRSS.c

--- ext/tunemygc/getRSS.c
+++ ext/tunemygc/getRSS.c
@@ -105,7 +105,7 @@ size_t getCurrentRSS( )
 	return (size_t)info.resident_size;
 #else
 	struct task_basic_info info;
-	mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT
+	mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT;
 	if (task_info( mach_task_self(), TASK_BASIC_INFO,
 		(task_info_t)&info, &infoCount) != KERN_SUCCESS)
 		return (size_t)0L;
